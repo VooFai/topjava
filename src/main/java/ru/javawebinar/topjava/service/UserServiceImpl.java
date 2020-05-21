a37f4afc0d847a7a5e17e05f8c681a10ee2ad2a9
voofai <temporant@mail.ru>
2020-05-21 17:22:36 +0500

    HW05-tests fix & separate profiles

--- src/main/java/ru/javawebinar/topjava/service/UserServiceImpl.java
+++ src/main/java/ru/javawebinar/topjava/service/UserServiceImpl.java
@@ -67,4 +67,9 @@ public class UserServiceImpl implements UserService {
     public void evictCache() {
         // only for evict cache
     }
+
+    @Override
+    public User getWithMeals(int id) {
+        return checkNotFoundWithId(repository.getWithMeals(id), id);
+    }
 }
---
+++ src/test/java/ru/javawebinar/topjava/service/AbstractMealServiceTest.java
@@ -0,0 +1,80 @@
+package ru.javawebinar.topjava.service;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+import static ru.javawebinar.topjava.MealTestData.*;
+import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
+import static ru.javawebinar.topjava.UserTestData.USER_ID;
+
+public abstract class AbstractMealServiceTest extends AbstractServiceTest {
+
+    @Autowired
+    protected MealService service;
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    public void testDelete() throws Exception {
+        service.delete(MEAL1_ID, USER_ID);
+        assertMatch(service.getAll(USER_ID), MEAL6, MEAL5, MEAL4, MEAL3, MEAL2);
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void testDeleteNotFound() throws Exception {
+        service.delete(MEAL1_ID, 1);
+    }
+
+    @Test
+    public void testCreate() throws Exception {
+        Meal mockData = getCreated();
+        Meal created = service.create(mockData, USER_ID);
+        mockData.setId(created.getId());
+        mockData.setUser(userService.get(USER_ID));
+
+        assertMatch(service.getAll(USER_ID), mockData, MEAL6, MEAL5, MEAL4, MEAL3, MEAL2, MEAL1);
+    }
+
+    @Test
+    public void testGet() throws Exception {
+        Meal actual = service.get(ADMIN_MEAL_ID, ADMIN_ID);
+        assertMatch(actual, ADMIN_MEAL1);
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void testGetNotFound() throws Exception {
+        service.get(MEAL1_ID, ADMIN_ID);
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+        Meal mockData = getUpdated();
+        Meal updated = service.update(mockData, USER_ID);
+        mockData.setId(updated.getId());
+        mockData.setUser(userService.get(USER_ID));
+        assertMatch(service.get(MEAL1_ID, USER_ID), mockData);
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void testUpdateNotFound() throws Exception {
+        service.update(MEAL1, ADMIN_ID);
+    }
+
+    @Test
+    public void testGetAll() throws Exception {
+        assertMatch(service.getAll(USER_ID), MEALS);
+    }
+
+    @Test
+    public void testGetBetween() throws Exception {
+        assertMatch(service.getBetweenDates(
+                LocalDate.of(2015, Month.MAY, 30),
+                LocalDate.of(2015, Month.MAY, 30), USER_ID), MEAL3, MEAL2, MEAL1);
+    }
+}
