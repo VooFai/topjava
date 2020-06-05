728a1ae2180f83d4a6dd9a91d50ce002cd4e971c
voofai <temporant@mail.ru>
2020-06-05 19:19:38 +0500

    fix

--- src/test/java/ru/javawebinar/topjava/service/AbstractJpaUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractJpaUserServiceTest.java
@@ -22,13 +22,4 @@ abstract public class AbstractJpaUserServiceTest extends AbstractUserServiceTest
         super.setUp();
         jpaUtil.clear2ndLevelHibernateCache();
     }
-
-    @Test
-    public void testValidation() throws Exception {
-        validateRootCause(() -> service.create(new User(null, "  ", "mail@yandex.ru", "password", Role.ROLE_USER)), ConstraintViolationException.class);
-        validateRootCause(() -> service.create(new User(null, "User", "  ", "password", Role.ROLE_USER)), ConstraintViolationException.class);
-        validateRootCause(() -> service.create(new User(null, "User", "mail@yandex.ru", "  ", Role.ROLE_USER)), ConstraintViolationException.class);
-        validateRootCause(() -> service.create(new User(null, "User", "mail@yandex.ru", "password", 9, true, new Date(), Collections.emptySet())), ConstraintViolationException.class);
-        validateRootCause(() -> service.create(new User(null, "User", "mail@yandex.ru", "password", 10001, true, new Date(), Collections.emptySet())), ConstraintViolationException.class);
-    }
 }
