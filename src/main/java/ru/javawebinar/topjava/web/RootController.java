38aad0c02203dc2ddace61fc3055c8cd0d4b253d
voofai <temporant@mail.ru>
2020-06-09 11:19:27 +0700

    7 0 4 mapping change

--- src/main/java/ru/javawebinar/topjava/web/RootController.java
+++ src/main/java/ru/javawebinar/topjava/web/RootController.java
@@ -4,8 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import ru.javawebinar.topjava.AuthorizedUser;
 import ru.javawebinar.topjava.service.MealService;
 import ru.javawebinar.topjava.service.UserService;
@@ -21,18 +21,18 @@ public class RootController {
     @Autowired
     private MealService mealService;
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @GetMapping("/")
     public String root() {
         return "index";
     }
 
-    @RequestMapping(value = "/users", method = RequestMethod.GET)
+    @GetMapping("/users")
     public String users(Model model) {
         model.addAttribute("users", userService.getAll());
         return "users";
     }
 
-    @RequestMapping(value = "/users", method = RequestMethod.POST)
+    @PostMapping("/users")
     public String setUser(HttpServletRequest request) {
         int userId = Integer.valueOf(request.getParameter("userId"));
         AuthorizedUser.setId(userId);
