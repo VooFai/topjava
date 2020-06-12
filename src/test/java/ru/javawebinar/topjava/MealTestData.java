fd99cb106196767e2edd7ff633df1ab858899e55
voofai <temporant@mail.ru>
2020-06-12 18:40:51 +0700

    8 02 HW07 rest controller

--- src/test/java/ru/javawebinar/topjava/MealTestData.java
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -2,6 +2,7 @@ package ru.javawebinar.topjava;
 
 import ru.javawebinar.topjava.matcher.BeanMatcher;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.to.MealWithExceed;
 
 import java.time.Month;
 import java.util.Arrays;
@@ -33,6 +34,7 @@ public class MealTestData {
     }
 
     public static final BeanMatcher<Meal> MATCHER = BeanMatcher.of(Meal.class);
+    public static final BeanMatcher<MealWithExceed> MATCHER_WITH_EXCEED = BeanMatcher.of(MealWithExceed.class);
 
    /* public static class MATCHER {
         public static void assertEquals(Meal actual, Meal expected) {
